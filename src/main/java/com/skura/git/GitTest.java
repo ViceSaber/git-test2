4b20dfc205b1c532c8829f9bd00253c95026b8c5
ViceSaber <113197017+ViceSaber@users.noreply.github.com>
2022-09-28 18:26:40 +0800

    pull commit

--- src/main/java/com/skura/git/GitTest.java
+++ src/main/java/com/skura/git/GitTest.java
@@ -9,7 +9,7 @@ public class GitTest {
         System.out.println("3333");
         //
         System.out.println("push again");
-        System.out.println("push again2");
-
+        System.out.println("push again2222");
+        
     }
 }
