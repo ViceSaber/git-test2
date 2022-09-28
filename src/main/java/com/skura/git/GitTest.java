e4e51074012d46e6c10cc12fe96e8f53dc4f5ffc
Skura <skura@jojo.com>
2022-09-28 16:35:32 +0800

    2.0 commit

--- src/main/java/com/skura/git/GitTest.java
+++ src/main/java/com/skura/git/GitTest.java
@@ -3,5 +3,6 @@ package com.skura.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello myWorld");
+        System.out.println("hi hi hi");
     }
 }
