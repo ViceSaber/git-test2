6e1b19e4694417d8d2275680fbd2a6461a868044
Skura <skura@jojo.com>
2022-09-28 16:40:42 +0800

    2.1 commit

--- src/main/java/com/skura/git/GitTest.java
+++ src/main/java/com/skura/git/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello myWorld");
         System.out.println("hi hi hi");
+        //sleep 1min..
+        System.out.println("2222");
     }
 }
