f619d97dfa22d857875594912b535fa809b5b5bf
Skura <skura@jojo.com>
2023-03-23 16:40:24 +0800

    3/23 1.0

---
+++ src/main/java/com/skura/git/Test3.java
@@ -0,0 +1,11 @@
+package com.skura.git;
+
+/**
+ * @Author 59264
+ * @Date 2023/3/23
+ */
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println("skura");
+    }
+}
