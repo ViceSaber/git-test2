b0ee7179923ce7c352a47db3932169e7b4c6e2a4
ViceSaber <113197017+ViceSaber@users.noreply.github.com>
2023-03-23 16:31:27 +0800

    Update GitTest.java

--- src/main/java/com/skura/git/GitTest.java
+++ src/main/java/com/skura/git/GitTest.java
@@ -4,12 +4,10 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello myWorld");
         System.out.println("hi hi hi");
-        //sleep 1min..
-        System.out.println("2222");
-        System.out.println("3333");
-        //
-        System.out.println("push again");
-        System.out.println("push again2222");
+        
+        //该分支即将被合并
+        System.out.println("the world will be changed in recent min");
+        
         
     }
 }
